7ac0c5a382c1b15cef65e07cb9e2851d6ca147c3
ming <wangym02@jcgroup.com.cn>
2017-11-13 18:22:57 +0800

    update

---
+++ src/Swiper/Slider.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import style from './style.css'
+
+export default class Slider extends Slider{
+  render(){
+    <div className={style['slider']}>
+
+    </div>
+  }
+}
--- src/Swiper/Swiper.js
+++ src/Swiper/Swiper.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react"
 import style from './style.css'
+import Slider from './Slider'
 
 export default class Swiper extends Component{
+
+  renderSliderList = () => {
+    const { children } = this.props
+    return (
+      <div className={style['slider-list']}>
+        {
+          children.map( (child, index) => {
+            return (
+              <Slider
+                key={index}
+              >
+                {child}
+              </Slider>  
+            )
+          })
+        }
+      </div>
+    )
+  }
+
   render(){
     const { children } = this.props
     return (
       <div className={style.container}>
-        <div className={style['slider-list']}>
-          {children}
-        </div>
+
       </div>
     )
   }
